3ea4b137e00db8f89d9d0f2742aa8a694703fc99
Josh Bickley-Wallace <joshbickleywallace@outlook.com>
2018-09-26 12:29:06 +0100

    Open te browser when the server starts

--- scripts/start.js
+++ scripts/start.js
@@ -2,9 +2,9 @@ process.env.BABEL_ENV = "development";
 process.env.NODE_ENV = "development";
 
 const chalk = require("chalk");
+const open = require("open");
 const webpack = require("webpack");
 const WebpackDevServer = require("webpack-dev-server");
-
 const config = require("../config/webpack.config.dev");
 
 const readyForReloadConfig = {
@@ -28,14 +28,16 @@ const devServer = new WebpackDevServer(compiler, serverConfig);
 
 const PORT = parseInt(process.env.PORT, 10) || 3000;
 const HOST = process.env.HOST || "0.0.0.0";
-const asFriendlyUrl = (host) => host === "0.0.0.0" ? "localhost" : host;
+const friendlyUrl = `http://${HOST === "0.0.0.0" ? "localhost" : HOST}:${PORT}/`;
 
 devServer.listen(PORT, HOST, (err) => {
   if (err) {
     return console.log(err);
   }
 
-  console.log(chalk.cyan(`Starting the development server...\nhttp://${asFriendlyUrl(HOST)}:${PORT}/`));
+  console.log(chalk.cyan(`Serving at ${friendlyUrl}\n`));
+  console.log(chalk.cyan("Opening browser..."));
+  open(friendlyUrl);
 });
 
 ["SIGINT", "SIGTERM"].forEach(function (sig) {
