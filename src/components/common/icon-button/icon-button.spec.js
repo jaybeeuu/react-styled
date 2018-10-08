43d5677c1e658388deeeba350b51b62360cfc4f2
Josh Bickley-Wallace <joshbickleywallace@outlook.com>
2018-10-08 21:35:41 +0100

    Add a test for prevent default

--- src/components/common/icon-button/icon-button.spec.js
+++ src/components/common/icon-button/icon-button.spec.js
@@ -16,13 +16,27 @@ describe("icon-button", () => {
     />
   );
 
+  const getEvent = () => ({
+    id: "{event}",
+    preventDefault: jest.fn().mockName("preventDefault")
+  });
+
   it("calls the onClick function when the element is clicked.", () => {
     const onClick = jest.fn().mockName("{prop_onClick}");
     const iconButton = getIconButton({ onClick });
-    const event = { id: "{event}" };
+    const event = getEvent();
     iconButton.simulate("click", event);
 
     expect(onClick).toHaveBeenCalledWith(event);
+  });
+
+  it("calls the preventDefault on the event when the element is clicked.", () => {
+    const onClick = jest.fn().mockName("{prop_onClick}");
+    const iconButton = getIconButton({ onClick });
+    const event = getEvent();
+    iconButton.simulate("click", event);
+
+    expect(event.preventDefault).toHaveBeenCalled();
   });
 
   it("renders the icon prop into a span.", () => {
